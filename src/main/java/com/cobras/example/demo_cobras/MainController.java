296e9bc3e750db3a0e5342954e8360c21b58b6c0
Tim Lieberman <tim.lieberman.1@gmail.com>
2022-05-23 01:54:58 -0400

    adding screen recording finalizing

--- src/main/java/com/cobras/example/demo_cobras/MainController.java
+++ src/main/java/com/cobras/example/demo_cobras/MainController.java
@@ -13,9 +13,9 @@ public class MainController {
     TextArea scrambleInputField;
 
     /**
-     * Returns random number within range.
-     * @param min
-     * @param max
+     * Returns random number with in range of min max
+     * @param min The starting point of a range
+     * @param max The end point of a range of numbers
      * @return
      */
     public int getRandomNumber(int min, int max) {
@@ -23,20 +23,35 @@ public class MainController {
     }
 
     /**
-     * Checks if input is an int or not.
-     * @param input
-     * @return
+     * Checks if input is an int or not. Returns boolean
+     * @param input The string in question
+     * @return boolean
      */
     protected boolean isInt(String input) {
         try {
-            Integer.parseInt(input);
+            Integer.parseInt(input.replaceAll("[^a-zA-Z0-9]", ""));
             return true;
         } catch (NumberFormatException e) {
             return false;
         }
     }
-    protected void handleOnPuncDet(Boolean x, String y) {
 
+    @FXML
+    protected void onClearButtonClick() {
+        String str = scrambleInputField.getText();
+        if (!isInt(str)) {
+            scrambleInputField.setText("Input must be a of type int");
+        }
+        Integer x = 0;
+        Integer target = Integer.parseInt(str);
+        scrambleInputField.setText(getSum(target).toString());
+    }
+
+    protected Integer getSum(Integer x) {
+        if (x == 0) {return 0;}
+        if( x % 3 == 0 || x % 5 == 0)
+        {return x + getSum(x-1);}
+        return getSum(x-1);
     }
 
     /**
@@ -48,9 +63,9 @@ public class MainController {
         String[] a = str.split(" ");
         String specialChar = "";
         String res = "";
-        // Marker for knowing if punctiation is required.
+        // Marker for determining if punctuation is required.
         boolean hasPeriod = false;
-        // Marker for when a capital letter is required.
+        // Marker for determining if a capital letter is required.
         boolean capNext = true;
 
         for (String aa : a) {
@@ -58,43 +73,37 @@ public class MainController {
                 res = res.concat(aa.concat(" "));
                 continue;
             }
+
             int y;
             int i = 0;
             String[] b = aa.split("");
             List<Integer> list = new ArrayList<>();
+            // Definetly not the best for performance but does guarentee a shuffle.
             while (i < b.length) {
                 y = getRandomNumber(0, b.length);
                 // This guarantees letters get shuffled.
-                if(b.length >= 2 && i == 0) {
-                    y = getRandomNumber(1, b.length);
-                }
+                if(b.length >= 2 && i == 0) {y = getRandomNumber(1, b.length);}
 
-                if (list.contains(y) ) {
-                    continue;
-                }
+                if (list.contains(y)) {continue;}
 
                 i++;
 
                 list.add(y);
                 switch (b[y]) {
                     case ":" -> {
-                        specialChar = ":";
-                        hasPeriod = true;
+                        specialChar = ":"; hasPeriod = true;
                         continue;
                     }
                     case "," -> {
-                        specialChar = ",";
-                        hasPeriod = true;
+                        specialChar = ","; hasPeriod = true;
                         continue;
                     }
                     case "." -> {
-                        specialChar = ".";
-                        hasPeriod = true;
+                        specialChar = "."; hasPeriod = true;
                         continue;
                     }
                     case "?" -> {
-                        specialChar = "?";
-                        hasPeriod = true;
+                        specialChar = "?"; hasPeriod = true;
                         continue;
                     }
                     default -> {
@@ -103,6 +112,7 @@ public class MainController {
 
                 b[y] = b[y].toLowerCase();
 
+                // Determine:"Does this word need to start with capital letter?"
                 if (capNext) {
                     b[y] = b[y].toUpperCase();
                     capNext = false;
@@ -110,10 +120,14 @@ public class MainController {
 
                 res = res.concat(b[y]);
             }
+
+            // If the word is last word in sentence the next word should have capital letter.
             if (hasPeriod) {
                 res = res.concat(specialChar);
                 hasPeriod = false;
-                if (specialChar.equals(".") || specialChar.equals(":")){
+                if (specialChar.equals(".") ||
+                        specialChar.equals(":") ||
+                        specialChar.equals("?")) {
                     capNext = true;
                 }
 
@@ -129,25 +143,6 @@ public class MainController {
         scrambleInputField.setText("Another point of interest is the break statement. Each break statement terminates the enclosing switch statement. Control flow continues with the first statement following the switch block. The break statements are necessary because without them, statements in switch blocks fall through: All statements after the matching case label are executed in sequence, regardless of the expression of subsequent case labels, until a break statement is encountered.");
     }
 
-    @FXML
-    protected void onClearButtonClick() {
-        String str = scrambleInputField.getText();
-        if (!isInt(str)) {
-            scrambleInputField.setText("Input must be a of type int");
-        }
-        Integer x = 0;
-        Integer target = Integer.parseInt(str);
-        scrambleInputField.setText(getSum(target).toString());
-    }
 
-    protected Integer getSum(Integer x) {
-        if (x == 0) {
-            return 0;
-        }
-        if( x % 3 == 0 || x % 5 == 0) {
-            return x + getSum(x-1);
-        }
-        return getSum(x-1);
-    }
 ;
 }
